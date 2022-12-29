722fb0b87c19d6b8192b47bded745cae44307e7a
Brabli <67018167+Brabli@users.noreply.github.com>
2022-12-29 15:14:07 +0000

    Add bits

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let file_path = &args[2];
 
-    dbg!(query, file_path);
+    let file_text = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("{}", file_text);
 }
